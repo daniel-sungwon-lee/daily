ac183960330a632a44309678243041f62d8dec52
Daniel Sungwon Lee <danlee333@gmail.com>
2021-04-15 13:40:32 -0700

    info popover

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { AppBar, Fade, Toolbar } from '@material-ui/core';
+import React, { useState } from 'react';
+import { AppBar, Fade, Popover, Toolbar } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles({
   nav: {
     backgroundColor: "#788DFF"
+  },
+  popover: {
+    width: "14rem",
+    marginTop: "1.25rem"
   }
 })
 
 export default function Nav(props) {
   const classes = useStyles()
+  const [info, setInfo] = useState(false)
+  const [anchor, setAnchor] = useState(null)
+
+  const handleInfo = (event) => {
+    setAnchor(event.currentTarget)
+    setInfo(true)
+  }
 
   return (
     <Fade in>
@@ -17,20 +28,29 @@ export default function Nav(props) {
         <AppBar position="sticky" classes={{root: classes.nav}}>
           <Toolbar className="justify-content-between">
 
-            <a href="/" className="text-decoration-none">
+            <a href="/" className="text-decoration-none mx-2">
               <div>
                 <img src="images/daily.svg" alt="Daily logo" width="65" />
               </div>
             </a>
 
-            <div className="d-flex">
-              <p className="mx-3 my-0 nav-p">
-                new routine
+            <div className="d-flex mx-2">
+              <p className="mr-2 my-0 nav-p">
+                new
               </p>
-              <p className="mx-3 my-0 nav-p">
+              <p className="mx-4 my-0 nav-p" onClick={handleInfo}>
                 info
               </p>
-              <a href="#top" className="text-decoration-none mx-3 nav-p">
+
+              <Popover open={info} anchorEl={anchor} onClose={() => setInfo(false)}
+               anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }} classes={{paper: classes.popover}}>
+                <p className="my-3 mx-4">
+                  Daily helps you create and schedule your daily routine; create a new
+                  routine now!
+                </p>
+              </Popover>
+
+              <a href="#top" className="text-decoration-none nav-p ml-2">
                 top
               </a>
             </div>
