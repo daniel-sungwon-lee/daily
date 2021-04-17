a70295f8b550761fab1478a24f1c7d8be218ca95
Daniel Sungwon Lee <danlee333@gmail.com>
2021-04-17 12:57:51 -0700

    added time pickers

--- client/src/components/new.jsx
+++ client/src/components/new.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Dialog, DialogTitle, DialogContent, DialogActions, TextField,
          Grow, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { KeyboardTimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
 const useStyles = makeStyles({
   dialog: {
@@ -18,6 +20,8 @@ const useStyles = makeStyles({
 export default function New(props) {
   const classes = useStyles();
   const { open, setOpen } = props;
+  const [from, setFrom] = useState(new Date())
+  const [to, setTo] = useState(new Date())
 
   return (
     <Dialog open={open} onClose={() => setOpen(false)} classes={{ paper: classes.dialog }}
@@ -30,7 +34,20 @@ export default function New(props) {
 
         <form>
           <DialogContent>
-            <TextField multiline label="Action" helperText="Ex: Work" color="secondary" />
+
+            <div>
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+
+                <KeyboardTimePicker label="From" placeholder="3:00 PM" mask="__:__ _M"
+                 value={from} onChange={date => setFrom(date)} />
+                <KeyboardTimePicker label="To" placeholder="6:00 PM" mask="__:__ _M"
+                 value={to} onChange={date => setTo(date)} />
+
+              </MuiPickersUtilsProvider>
+            </div>
+
+            <TextField multiline label="Action" helperText="Ex: Code personal projects" color="secondary" />
+
           </DialogContent>
 
           <DialogActions>
