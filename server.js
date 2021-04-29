23890c8ab2fe60c59242768b2558f8f7105a4a67
Daniel Sungwon Lee <danlee333@gmail.com>
2021-04-29 02:15:35 -0700

    updated server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 require('dotenv/config');
 const express = require('express');
 const pg = require('pg');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
 const path = require('path');
 const port = process.env.PORT || 3001;
 
@@ -44,6 +46,60 @@ function errorMiddleware(err, req, res, next) {
 }
 
 //API endpoints
+
+//auth
+app.post('/api/signup', (req, res, next) => {
+  const { email, password } = req.body;
+
+  argon2
+    .hash(password)
+    .then(hashedPassword => {
+      const sql = `
+     insert into "users" ("email", "hashedPassword")
+     values ($1, $2)
+     `;
+      const params = [email, hashedPassword];
+
+      db.query(sql, params)
+        .then(result => {
+          res.status(201).json(result.rows[0]);
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
+
+app.post('/api/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  const sql = `
+  select "userId", "hashedPassword", "email"
+  from "users"
+  where "email" = $1
+  `;
+  const params = [email];
+
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'Invalid login');
+      }
+      const { userId, hashedPassword, email } = user;
+      argon2
+        .verify(hashedPassword, password)
+        .then(isMatch => {
+          if (!isMatch) {
+            throw new ClientError(401, 'Invalid login');
+          }
+          const payload = { userId, email };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
 
 //add new
 app.post('/api/routines', (req, res, next) => {
