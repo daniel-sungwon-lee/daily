a1a033de38dcb741588d34f42a8cf020aa4366f1
Daniel Sungwon Lee <danlee333@gmail.com>
2021-05-01 13:24:34 -0700

    auth WIP

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,51 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Nav from './components/nav';
 import Progress from './components/progress';
 import Home from './pages/home';
 import Auth from './pages/auth';
+import decodeToken from './decodeToken';
 
 function App() {
   const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("dailyUserToken")
+    const user = token ? decodeToken(token) : null
+
+    setUser(user)
+  }, [])
+
+  const handleLogin = (result) => {
+    setLoading(true)
+    const { user, token } = result
+
+    setUser(user)
+    window.localStorage.setItem("dailyUserToken", token)
+
+    if (window.localStorage.getItem("dailyUserToken")) {
+      window.location.pathname = "/"
+    }
+  }
+
+  const handleSignOut = () => {
+    window.localStorage.removeItem("dailyUserToken")
+    setUser(null)
+  }
+
+  if (!user) {
+    return (
+      <div className="App">
+        <Auth handleLogin={handleLogin} />
+      </div>
+    )
+  }
+
+  if (window.location.pathname === "/auth" && user) {
+    window.location.pathname = "/"
+  }
 
   return (
     <div className="App">
@@ -20,12 +58,12 @@ function App() {
          }
         </>
 
-        <Nav />
+        <Nav handleSignOut={handleSignOut} />
 
         <Switch>
 
         <Route exact path="/auth">
-          <Auth />
+          <Auth handleLogin={handleLogin} />
         </Route>
 
         <Route exact path="/">
