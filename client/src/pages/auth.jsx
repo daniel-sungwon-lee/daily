40ac440382f7fd4019f3c9562f27dca341fbfca8
Daniel Sungwon Lee <danlee333@gmail.com>
2021-05-01 19:18:25 -0700

    rendered login page

--- client/src/pages/auth.jsx
+++ client/src/pages/auth.jsx
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
-import { Grow } from '@material-ui/core';
+import { Grow, TextField, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  dialog: {
+    width: "75%",
+    borderRadius: "1rem"
+  },
+  button: {
+    textTransform: "lowercase",
+    fontSize: "18px",
+    borderRadius: "0.5rem"
+  }
+})
 
 export default function Auth(props) {
+  const classes = useStyles();
   const [page, setPage] = useState('login')
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -76,8 +90,29 @@ export default function Auth(props) {
   if(page === 'login') {
     return (
       <>
-        <Grow>
+        <Grow in>
           <div className="container">
+
+            <div className="m-3">
+              <img src="/images/daily.svg" alt="LightBlb logo" width="200" />
+            </div>
+
+            <form className="d-flex flex-column align-items-center" onSubmit={handleLogin}>
+
+              <TextField id="email" label="Email" required InputLabelProps={{ required: false }}
+                type="email" value={email} className="mb-4" onChange={handleChange} error={error}
+                color="secondary" />
+
+              <TextField id="password" label="Password" required InputLabelProps={{ required: false }}
+                type="password" value={password} className="mb-4" onChange={handleChange} error={error}
+                helperText={errText} color="secondary" />
+
+              <Button className="mt-2" type="submit" classes={{ text: classes.button }}
+                style={{ color: "#4F5DFF" }}>
+                login
+              </Button>
+
+            </form>
 
           </div>
         </Grow>
