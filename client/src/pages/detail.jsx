a4bbb2d4ef157cb0302b3e614364bd45b182eb3b
Daniel Sungwon Lee <danlee333@gmail.com>
2021-05-19 13:45:39 -0700

    detail page WIP

--- client/src/pages/detail.jsx
+++ client/src/pages/detail.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Grow, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Placeholder from '../components/placeholder';
+
+const useStyles = makeStyles({
+  paper: {
+    padding: "3rem",
+    borderRadius: "1rem"
+  }
+})
 
 export default function Detail(props) {
+  const classes = useStyles();
+  const [show, setShow] = useState(true)
+
+  useEffect(() => {
+    setShow(false)
+  }, [])
+
   return (
-    <div></div>
+    <div className="container">
+      <div>
+        {
+          show ? <Placeholder />
+               : <>
+                  <Grow in>
+                    <Paper className={classes.paper}>
+
+                    </Paper>
+                  </Grow>
+                 </>
+        }
+      </div>
+    </div>
   )
 }
